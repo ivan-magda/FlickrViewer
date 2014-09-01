d45fcfa93af09ed79587be453685af8679b76677
Ivan Magda <imagda15@gmail.com>
2014-09-01 22:20:13 +1000

    Initial Commit

---
+++ FlickrViewer/PSRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSRAppDelegate.h
+//  FlickrViewer
+//
+//  Created by Ivan Magda on 01.09.14.
+//  Copyright (c) 2014 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrViewer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlickrViewer
+//
+//  Created by Ivan Magda on 01.09.14.
+//  Copyright (c) 2014 Ivan Magda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSRAppDelegate class]));
+    }
+}
